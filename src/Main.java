981959679bf5f303d1c54f792b11109b1df19ee9
ambrishba-sacumen <ambrish.ba@sacumen.com>
2023-04-28 14:24:50 +0530

    removed java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        String s="new java";
+        String s="n";
         System.out.println(s);
     }
 }
